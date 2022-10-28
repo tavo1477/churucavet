4b73681e5abedacb28b63d3fe16db5f8a7f70169
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-10-28 15:58:08 -0600

    Vinculando el controlador de correo y el autoload de PHPmailer

--- index.php
+++ index.php
@@ -9,5 +9,8 @@ require_once "models/servicesModel.php";
 require_once "controllers/infoServiciosController.php";
 require_once "models/infoServiciosModel.php";
 
+require_once "controllers/correoControlador.php";
+require_once "extensiones/vendor/autoload.php";
+
 $plantilla = new ControllerTemplate();
 $plantilla -> ctrTemplate();
