a66fb425f7f0ccb828c666134de57315ff4c7092
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-11-01 11:40:46 -0600

    Cambiando 3 imágenes en el slider del slideshow

--- views/paginas/modulos/slider.php
+++ views/paginas/modulos/slider.php
@@ -12,10 +12,10 @@
 
             <div class="textoSlide">
 
-                <h4>El cuidado comienza aquí</h4>
-                <h1>Cuidado especial para tus mascotas</h1>                
+                <h4 class="wow animated bounceInLeft" data-wow-delay="0.3s" data-wow-duration=".2s">El cuidado comienza aquí</h4>
+                <h1 class="wow animated bounceInLeft" data-wow-delay="0.5s" data-wow-duration=".4s">Cuidado especial para tus mascotas</h1>                
 
-                <a href="<?php echo $route ?>/contacto" class="button-link center-block">
+                <a href="<?php echo $route ?>/contacto" class="button-link center-block wow animated fadeInDown" data-wow-delay="0.5s" data-wow-duration=".4s">
 
                      <button class="button-primary">Reservar una cita</button>               
 
@@ -31,8 +31,8 @@
 
             <div class="textoSlide">
 
-                <h4>El cuidado comienza aquí</h4>
-                <h1>Atención de calidad para mascotas</h1>  
+                <h4>Nosotros le damos estilo a tu mascota</h4>
+                <h1>El mejor estilo de grooming para gatos y perros de muchas razas.</h1>  
 
                 <a href="<?php echo $route ?>/contacto" class="button-link center-block">
 
