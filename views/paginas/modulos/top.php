f08c9a8de12e7f8b1a3442cba38c57d84d79c585
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-11-07 18:10:20 -0600

    Agregando pestaña de descargables al menú

--- views/paginas/modulos/top.php
+++ views/paginas/modulos/top.php
@@ -41,9 +41,67 @@
 HEADER
 ======================================-->
 
-<header class="main-header">
+<header class="main-header l-container">
+
+		<a class="logo" href="<?php echo $route ?>">
+			
+			<img src="<?php echo $route ?>views/images/logo-churucavet.png" alt="Logo Churucavet">
+
+		</a>
+
+		<nav class="navbar">
+
+			<ul class="navbar-menu">
+
+				<li class="navbar-menu__item">
+
+					<a href="<?php echo $route ?>" class="navbar-menu__link"><i class="fa-solid fa-house-user"></i></a>
+					
+				</li>
+
+				<li class="navbar-menu__item">
+
+					<a href="acerca-de-la-clinica" class="navbar-menu__link">Acerca de la clínica</a>
+					
+				</li>
+
+				<li class="navbar-menu__item">
+
+					<a href="acerca-de-la-clinica" class="navbar-menu__link">Servicios</a>
+					
+				</li>
+
+				<li class="navbar-menu__item">
+
+					<a href="#" class="navbar-menu__link">Descargables <i class="fa-solid fa-caret-down"></i>
+
+						<ul class="submenu">
+							
+							<li class="submenu-item">
+								
+								<a href="https://www.mediafire.com/file/2js6vb95z7o18yx/CUIDADOS_POST_OPERATORIOS_CHURUCAVET.pdf/file"target="_blank" class="submenu-item__link">Cuidados post operatorios</a>
+
+							</li>
+
+						</ul>
+
+					</a>
+					
+				</li>
+
+				<li class="navbar-menu__item">
+
+					<a href="contacto" class="navbar-menu__link">Contacto</a>
+					
+				</li>
+				
+
+			</ul>
+			
+
+		</nav>
 	
-	<section class="main-header__block l-container">
+	<!-- <section class="main-header__block l-container">
 
 		<a href="<?php echo $route ?>">
 			
@@ -73,7 +131,19 @@ HEADER
 
 					<a href="servicios" class="main-menu__link">Servicios</a>
 					
-				</li>						
+				</li>
+
+				<li class="main-menu__item">
+
+					<a href="servicios" class="main-menu__link">Descargables <i class="fa-solid fa-caret-down"></i></a>
+
+					<ul class="main-submenu">
+
+						<li class="main-submenu__subitem"></li>
+						
+					</ul>
+					
+				</li>							
 				
 				<li class="main-menu__item">
 
@@ -85,6 +155,6 @@ HEADER
 
 		</nav>		
 
-	</section>
+	</section> -->
 
 </header>
