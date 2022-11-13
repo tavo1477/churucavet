26ca63d20dd667a29fd386d93ed0cd5a8f86f73d
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-11-13 08:33:02 -0600

    Cambiando el estilo del header

--- views/paginas/modulos/top.php
+++ views/paginas/modulos/top.php
@@ -1,160 +1,60 @@
 <!--=====================================
- TOP
+HEADER
  ======================================-->
-<div class="top l-container">
 
-	<div class="top-called">
+<nav>
+		<div class="logo">
 
-		<h3 class="top-title"><strong>Puedes llamarnos al:</strong> <span class="text-color">(+506) 2551-6685</span></h3>
-
-	</div>
-
-	<div class="top-visited">
-		
-		<h3 class="top-title"><strong>Ubicación:</strong> <span class="text-color">400 oeste del parque de San Rafael de Oreamuno, Cartago </span></h3>
-
-	</div>
-
-	<div class="top-redesSociales">		
-
-		<div class="footer-redesSociales__links">			
-
-			<a href="https://www.facebook.com/profile.php?id=100063596321136" target="_blank">
-
-				<i class="fa-brands fa-facebook-f"></i>
-
-			</a>
-			
-			<a href="https://www.instagram.com/churucavetclinica_/" target="_blank">
-
-				<i class="fa-brands fa-instagram"></i>
-
-			</a>			
+			<img src="<?php echo $route?>views/images/logo-churucavet.png" alt="Logo Churucavet">
 
 		</div>
 
-	</div>	
+		<button type="button" class="btn-hamburger" data-action="nav-toggle">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</button>
+		<ul class="nav-menu">
 
-</div>
+			<li class="nav-item">
 
-<!--=====================================
-HEADER
-======================================-->
+				<a href="<?php echo $route?>">Inicio</a>
 
-<header class="main-header l-container">
+			</li>
 
-		<a class="logo" href="<?php echo $route ?>">
-			
-			<img src="<?php echo $route ?>views/images/logo-churucavet.png" alt="Logo Churucavet">
 
-		</a>
+			<li class="nav-item">
 
-		<nav class="navbar">
+				<a href="acerca-de-la-clinica">Acerca de la clínica</a>
 
-			<ul class="navbar-menu">
+			</li>
 
-				<li class="navbar-menu__item">
+			<li class="nav-item">
 
-					<a href="<?php echo $route ?>" class="navbar-menu__link"><i class="fa-solid fa-house-user"></i></a>
+				<a href="servicios">Servicios</a>
+
+			</li>
+
+			<li class="nav-item dropdown">
+
+				<a href="#" data-action="dropdown-toggle">Descargables <i class="fa-solid fa-chevron-down"></i></a>
+
+				<div class="dropdown-menu">
+
+					<a class="dropdown-item" href="#">Cuidados PostOperatorios </a>
+
 					
-				</li>
+				</div>
 
-				<li class="navbar-menu__item">
+			</li>
 
-					<a href="acerca-de-la-clinica" class="navbar-menu__link">Acerca de la clínica</a>
-					
-				</li>
+			<li class="nav-item">
 
-				<li class="navbar-menu__item">
+				<a href="contacto">Contacto</a>
 
-					<a href="acerca-de-la-clinica" class="navbar-menu__link">Servicios</a>
-					
-				</li>
+			</li>
 
-				<li class="navbar-menu__item">
-
-					<a href="#" class="navbar-menu__link">Descargables <i class="fa-solid fa-caret-down"></i>
-
-						<ul class="submenu">
-							
-							<li class="submenu-item">
-								
-								<a href="https://www.mediafire.com/file/2js6vb95z7o18yx/CUIDADOS_POST_OPERATORIOS_CHURUCAVET.pdf/file"target="_blank" class="submenu-item__link">Cuidados post operatorios</a>
-
-							</li>
-
-						</ul>
-
-					</a>
-					
-				</li>
-
-				<li class="navbar-menu__item">
-
-					<a href="contacto" class="navbar-menu__link">Contacto</a>
-					
-				</li>
-				
-
-			</ul>
-			
-
-		</nav>
-	
-	<!-- <section class="main-header__block l-container">
-
-		<a href="<?php echo $route ?>">
-			
-			<img src="<?php echo $route ?>views/images/logo-churucavet.png" alt="Logo Churucavet">
-
-		</a>
-
-		<div class="main-menu-toggle" id="main-menu-toggle"></div>
-
-		<nav class="main-nav" id="main-nav">
-			
-			<ul class="main-menu">
-				
-				<li class="main-menu__item">
-
-					<a href="<?php echo $route ?>" class="main-menu__link">Inicio</a>
-					
-				</li>	
-
-				<li class="main-menu__item">
-
-					<a href="acerca-de-la-clinica" class="main-menu__link">Acerca de la clínica</a>
-					
-				</li>			
-
-				<li class="main-menu__item">
-
-					<a href="servicios" class="main-menu__link">Servicios</a>
-					
-				</li>
-
-				<li class="main-menu__item">
-
-					<a href="servicios" class="main-menu__link">Descargables <i class="fa-solid fa-caret-down"></i></a>
-
-					<ul class="main-submenu">
-
-						<li class="main-submenu__subitem"></li>
-						
-					</ul>
-					
-				</li>							
-				
-				<li class="main-menu__item">
-
-					<a href="contacto" class="main-menu__link">Contacto</a>
-					
-				</li>				
-
-			</ul>
-
-		</nav>		
-
-	</section> -->
-
-</header>
+		</ul>
+	</nav>
