6e38ae3672163528201f02c685803399790bdf65
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-11-22 16:10:53 -0600

    Agregando y maquetando la página de osdranick-ramirez-groomista.php

---
+++ views/paginas/osdranick-ramirez-groomista.php
@@ -0,0 +1,51 @@
+ <div class="equipo-app">
+
+	<main class="equipo-main l-container">
+		
+		<aside class="equipo-aside">
+
+			<div class="img-doctor">
+
+				<img src="<?php echo $route ?>views/images/groomista.jpg" alt="Foto Groomista">
+
+			</div>
+
+			<div class="equipo-aside__info">
+				
+				<h3 class="equipo-aside__info--name">Osdranick Ramirez</h3>				
+
+				<div class="equipo-aside__info--desc">					
+					
+					<p> Nuestra asistente y groomista, nos ayuda en los baños y chineos para sus mascotas. Osdra no solo se encarga de bañar y cortar pelaje, ella además inspecciona el estado de salud de los peluditos y recomienda la revisión del Dr en caso de requerirse.</p>
+
+				</div>
+
+			</div>			
+
+		</aside>
+
+		<section class="equipo-info">
+
+			<h3 class="equipo-info__title">Estudios de enriquecimiento profesional:</h3>
+
+			<ul class="equipo-info__list">
+				
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> Tècnico en Asistencia Veterinaria. Instituto IMA. Costa Rica. 2021</li>
+
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> Curso online internacional: Parasitosis intestinales en veterinaria. Academia Novedades. Colombia. Agosto 2022 </li>
+
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> Peluquería desde cero. 2Hands Art. Colombia 2022</li>
+
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> LABSA Week. Capacitaciòn. Agosto 2022</li>
+
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> Curso online de estètica canina profesional. Instituto técnico Pink Poodle. Enero 2022</li>
+
+			</ul>
+
+
+		</section>
+
+	</main>	
+
+</div>
+ 
