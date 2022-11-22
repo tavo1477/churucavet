09f1f8be17139f21c99607013900916b302807e5
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-11-22 16:10:27 -0600

    Agregando y maquetando la página de dr-gustavo-canales.php

---
+++ views/paginas/dr-gustavo-canales.php
@@ -0,0 +1,54 @@
+ <div class="equipo-app">
+
+	<main class="equipo-main l-container">
+		
+		<aside class="equipo-aside">
+
+			<div class="img-doctor">
+
+				<img src="<?php echo $route ?>views/images/medico-veterinario.jpeg" alt="Foto Doctor Veterinario">
+
+			</div>
+
+			<div class="equipo-aside__info">
+				
+				<h3 class="equipo-aside__info--name">Dr. Gustavo Andrès Canales Obando</h3>
+				<h4 class="equipo-aside__info--code">CMV 2045</h4>
+
+				<div class="equipo-aside__info--desc">
+					
+					<span>Estudios de grado:</span>
+
+					<p>Licenciatura en Medicina y Cirugìa Veterinaria, en Escuela de Medicina y Cirugìa Veterinaria San Francisco de Asìs, Universidad Veritas (2017).</p>
+
+				</div>
+
+			</div>			
+
+		</aside>
+
+		<section class="equipo-info">
+
+			<h3 class="equipo-info__title">Estudios de enriquecimiento profesional:</h3>
+
+			<ul class="equipo-info__list">
+				
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> Seminario de Anestesia y Analgesia Veterinaria. Universidad Veritas. Abril 2017</li>
+
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> 35th World Veterinary Association Congress. Abril 2019</li>
+
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> Primera Jornada de Medicina Interna y Urgencias IVES. Abril 2022</li>
+
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> LABSA Week. Capacitaciòn. Agosto 2022</li>
+
+				<li class="equipo-info--item"> <i class="fa-solid fa-circle"></i> Diplomado Anestesiologìa Veterinaria. VetPlus Academy. Octubre 2022</li>
+
+			</ul>
+
+
+		</section>
+
+	</main>	
+
+</div>
+ 
