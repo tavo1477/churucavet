6efc5a61920fe26268d98618079819e3016e3456
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-11-25 08:49:08 -0600

    Modificando la galería de imágenes

--- views/paginas/modulos/galeria.php
+++ views/paginas/modulos/galeria.php
@@ -8,278 +8,95 @@ GALERÍA
     
 </div>
 
-<section  class="galeria-container">
 
-    <div class="galeria-enlaces">
+<div class="galeria-container l-container">
 
-        <ul class="galeria-enlaces__block l-container">
-           
-            <li class="filter active" data-nombre="todos">Todos</li>
-            <li class="filter" data-nombre="consultas-medicas">Consultas médicas</li>
-            <li class="filter" data-nombre="domicilios">Domicilios</li>
-            <li class="filter" data-nombre="cirugias">Cirugías</li>
-            <li class="filter" data-nombre="capacitaciones">Capacitaciones</li>
-            <li class="filter" data-nombre="proceso-cirugia-ojo">Proceso de una cirugía en ojo</li>
-            <li class="filter" data-nombre="ultrasonidos">Ultrasonidos</li>
-            <li class="filter" data-nombre="animales-recuperados">Animales recuperados</li> 
+    <div class="galeria-item img-gal">
 
-        </ul>
+         <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/consultas-medicas1.jpg">
+             
+              <img src="<?php echo $route ?>views/images/galeria/consultas-medicas1.jpg" alt="Consultas Médicas">
+
+         </a>          
+
+    </div>  
+
+    <div class="galeria-item">
+
+         <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/consultas-medicas2.jpg">
+             
+            <img src="<?php echo $route ?>views/images/galeria/consultas-medicas2.jpg" alt="Consultas Médicas">
+
+         </a>          
         
+       
+    </div> 
 
-    </div>
-    
-    <!-- <div class="galeria-botones ">
+    <div class="galeria-item">
+
+        <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/consultas-medicas3.jpg">
+             
+            <img src="<?php echo $route ?>views/images/galeria/consultas-medicas3.jpg" alt="Consultas Médicas">
+
+         </a>    
+       
+    </div> 
+
+    <div class="galeria-item">
+
+        <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/cirugia1.jpg">
+             
+            <img src="<?php echo $route ?>views/images/galeria/cirugia1.jpg" alt="Cirugías">
+
+         </a>     
+       
+    </div> 
+
+    <div class="galeria-item">
+
+        <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/grooming1.jpg">
+             
+            <img src="<?php echo $route ?>views/images/galeria/grooming1.jpg" alt="Grooming">
+
+         </a>  
         
-        <ul class="botones-enlaces l-container">
-            
-            
+    </div> 
 
-        </ul>
+    <div class="galeria-item">
 
-    </div> -->
+         <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/domicilio1.jpg">
+             
+             <img src="<?php echo $route ?>views/images/galeria/domicilio1.jpg" alt="Consulta a domicilio">
 
-    <div class="galeria-images l-container">
+         </a>         
+      
+    </div> 
 
-       <!--  consultas-medicas -->
+    <div class="galeria-item">
 
-        <div class="cont-images consultas-medicas">
+        <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/limpieza-dental1.jpg">
+             
+             <img src="<?php echo $route ?>views/images/galeria/limpieza-dental1.jpg" alt="Limpieza dental">
 
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/consultas-medicas/consultas-medicas1.jpg">
-                
-                     <img src="<?php echo $route ?>views/images/galeria/consultas-medicas/consultas-medicas1.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div> 
-
-         <div class="cont-images consultas-medicas">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/consultas-medicas/consultas-medicas2.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/consultas-medicas/consultas-medicas2.jpg" alt="">
-
-                </a>
-
-            </div>   
-
-        </div> 
-
-        <!-- Domicilios -->
-
-        <div class="cont-images domicilios">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/domicilios/domicilio1.jpg">
-                
-                  <img src="<?php echo $route ?>views/images/galeria/domicilios/domicilio1.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div> 
-
-        <div class="cont-images domicilios">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/domicilios/domicilio2.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/domicilios/domicilio2.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div> 
-
-        <!-- Cirugías -->
-
-        <div class="cont-images cirugias">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/cirugias/cirugia1.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/cirugias/cirugia1.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div> 
-
-        <div class="cont-images cirugias">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/cirugias/cirugia2.jpg">
-                
-                 <img src="<?php echo $route ?>views/images/galeria/cirugias/cirugia2.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div>
-
-        <div class="cont-images cirugias">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/cirugias/cirugia3.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/cirugias/cirugia3.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div>
-
-         <!-- Capacitaciones -->
-
-         <div class="cont-images capacitaciones">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/capacitaciones/capacitacion1.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/capacitaciones/capacitacion1.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div>
-
-        <div class="cont-images capacitaciones">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/capacitaciones/capacitacion2.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/capacitaciones/capacitacion2.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div>
-
-
-        <!-- Proceso de una cirugía en ojo -->
-
-        <div class="cont-images proceso-cirugia-ojo">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/proceso-cirugia-ojo/proceso-cirugia-ojo1.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/proceso-cirugia-ojo/proceso-cirugia-ojo1.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div>
-
-        <div class="cont-images proceso-cirugia-ojo">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/proceso-cirugia-ojo/proceso-cirugia-ojo2.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/proceso-cirugia-ojo/proceso-cirugia-ojo2.jpg" alt="">
+         </a>         
                     
-                </a>
+    </div> 
 
-            </div>       
+    <div class="galeria-item">
 
-        </div>
+        <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/perrito-recuperado.jpg">
+             
+             <img src="<?php echo $route ?>views/images/galeria/perrito-recuperado.jpg" alt="Perrito recuperado">
 
-        <div class="cont-images proceso-cirugia-ojo">
+         </a>       
 
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/proceso-cirugia-ojo/proceso-cirugia-ojo3.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/proceso-cirugia-ojo/proceso-cirugia-ojo3.jpg" alt="">
-                    
-                </a>
-
-            </div>   
-
-        </div>        
-
-        <div class="cont-images proceso-cirugia-ojo">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/proceso-cirugia-ojo/proceso-cirugia-ojo4.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/proceso-cirugia-ojo/proceso-cirugia-ojo4.jpg" alt="">
-                    
-                </a>
-
-            </div>      
-
-        </div>
-
-        <!-- Ultrasonidos -->
-
-        <div class="cont-images ultrasonidos">
-
-            <div class="img-gal">
-
-                <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/ultrasonidos/ultrasonido1.jpg">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/ultrasonidos/ultrasonido1.jpg" alt="">
-
-                </a>
-
-            </div>        
-
-        </div>
-
-        <!-- Animales recuperados -->
-
-        <div class="cont-images animales-recuperados">
-
-            <div class="img-gal">
-
-                <a data-fancybox href="https://www.youtube.com/watch?v=yeKnClnZ9SE?feature=share">
-                
-                  <img src="<?php echo $route ?>views/images/galeria/animales-recuperados/perrito-recuperado1.png" alt="">
-
-                </a>
-
-            </div>        
-
-        </div>
-
-        <div class="cont-images animales-recuperados">
-
-            <div class="img-gal">
-
-                <a data-fancybox href="https://www.youtube.com/watch?v=0JqusEvL4dYE?feature=share">
-                
-                    <img src="<?php echo $route ?>views/images/galeria/animales-recuperados/perrito-recuperado2.png" alt="">
-
-                </a>
-
-            </div>        
-
-        </div>
+    </div> 
 
 
-    </div>
+        <a href="galeria" class="button-link center-block">
 
-</section>
+             <button class="button-primary">Más imágenes</button>               
+
+        </a>  
+
+</div>
