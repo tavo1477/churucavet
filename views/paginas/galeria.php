d861d7bf8a114224ffd3ba222341f51e079fcaed
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-11-25 10:06:51 -0600

    Maquetando la nueva página de galeria.php

---
+++ views/paginas/galeria.php
@@ -0,0 +1,81 @@
+ <!--=====================================
+ GALERÍA BANNER
+ ======================================-->
+
+ <div class="galeria-header">
+    
+     <div class="galeria-header__container l-container">
+        
+        <h3 class="galeria-header__title">Galería</h3>
+
+        <ul class="breadcrumb">
+
+          <li class="breadcrumb-item"><a href="<?php echo $route?>">Inicio</a></li>
+          <li>/</li>
+          <li class="breadcrumb-item">Galería</li>
+          
+        </ul>
+
+     </div>
+
+</div>
+
+ <!--=====================================
+ GALERÍA 
+ ======================================-->
+
+ <div class="galeria-title center-content">Nuestra galería</div>
+
+ <div class="galeria-container l-container">
+     
+    <div class="galeria-item img-gal">
+
+         <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/capacitacion1.jpg">
+             
+              <img src="<?php echo $route ?>views/images/galeria/capacitacion1.jpg" alt="Capacitaciones">
+
+         </a>          
+
+    </div>  
+
+    <div class="galeria-item img-gal">
+
+         <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/capacitacion2.jpg">
+             
+              <img src="<?php echo $route ?>views/images/galeria/capacitacion2.jpg" alt="Capacitaciones">
+
+         </a>          
+
+    </div>  
+
+    <div class="galeria-item img-gal">
+
+         <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/cirugia2.jpg">
+             
+              <img src="<?php echo $route ?>views/images/galeria/cirugia2.jpg" alt="Cirugías">
+
+         </a>          
+
+    </div>  
+
+    <div class="galeria-item img-gal">
+
+         <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/consultas-medicas4.jpg">
+             
+              <img src="<?php echo $route ?>views/images/galeria/consultas-medicas4.jpg" alt="Consultas médicas">
+
+         </a>          
+
+    </div> 
+
+    <div class="galeria-item img-gal">
+
+         <a data-fancybox="gallery" href="<?php echo $route ?>views/images/galeria/grooming2.jpg">
+             
+              <img src="<?php echo $route ?>views/images/galeria/grooming2.jpg" alt="Gromming">
+
+         </a>          
+
+    </div> 
+
+ </div>
